0a9072ba3d7a06b25bae9dd6f78cea4953ca1063
GitHub Action <action@github.com>
2021-10-17 16:05:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hirosneko/shadesmearjetbrains/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hirosneko.shadesmearjetbrains.services
+
+import com.github.hirosneko.shadesmearjetbrains.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
